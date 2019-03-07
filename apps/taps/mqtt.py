4b2809ed002eb051d639dc5656cd5a49137b6905
vincent <vincentalfred1@gmail.com>
2019-03-07 21:49:41 +0700

    edit mqtt

--- apps/taps/mqtt.py
+++ apps/taps/mqtt.py
@@ -2,25 +2,36 @@ import os
 import paho.mqtt.client as mqtt
 from django.utils import timezone
 
+machines = {}
+
 def on_connect(client, userdata, flags, rc):
-	print("CConnect to machine1")
-	client.subscribe("machine1")
+	from apps.taps.models import Tap
+	from apps.machines.models import Machine
+
+	global machines
+	machines = Machine.objects.get();
+	for machine in machines:
+		topic = "{}/state/#".format(machine.id)
+		client.subscribe(topic)
 
 def on_message(client, userdata, msg):
 	print("Yeay! payload = "+str(msg.payload))
 	msg.payload = msg.payload.decode("utf-8")
 	
-	from apps.taps.models import Tap
-	from apps.machines.models import Machine
+	for machine in machines:
+		topic = "{}/state/carduid".format(machine.id)
+		if msg.topic == topic:
+			print(msg.topic+" "+str(msg.payload))
+			card_uid = msg.payload
+			machine = Machine.objects.get(pk=1)
+			tap_time = timezone.now()
+			power_usage = 100
+			tap = Tap(card_uid = card_uid, machine = machine, tap_time = tap_time, power_usage = power_usage)
+			tap.save()
+		topic = "{}/state/power".format(machine.id)
+		if msg.topic == topic:
+			print(msg.topic+" "+str(msg.payload))
 
-	if msg.topic == "machine1":
-		print(msg.topic+" "+str(msg.payload))
-		card_uid = msg.payload
-		machine = Machine.objects.get(pk=1)
-		tap_time = timezone.now()
-		power_usage = 100
-		tap = Tap(card_uid = card_uid, machine = machine, tap_time = tap_time, power_usage = power_usage)
-		tap.save()
 
 def on_disconnect(client, userdata, rc):
 	client.loop_stop(force=False)
