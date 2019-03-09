a705bef5fac577219ad0d3d7088a8be4727c734b
vincent <vincentalfred1@gmail.com>
2019-03-09 17:43:20 +0700

    test

--- hac/settings.py
+++ hac/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'HOST': os.environ['DB_HOST'],
-        'PORT': os.environ['DB_PORT'],
+        # 'PORT': os.environ['DB_PORT'],
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASSWORD'],
