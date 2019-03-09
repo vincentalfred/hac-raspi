44e630474594a24c04cfaa949123b40ee7cca286
Vincent Alfred <vincentalfred1@gmail.com>
2019-03-09 19:21:32 +0700

    edit mqtt.py on message

--- apps/taps/mqtt.py
+++ apps/taps/mqtt.py
@@ -9,21 +9,25 @@ def on_connect(client, userdata, flags, rc):
 	from apps.machines.models import Machine
 
 	global machines
-	machines = Machine.objects.get();
+	machines = Machine.objects.all();
 	for machine in machines:
 		topic = "{}/state/#".format(machine.id)
 		client.subscribe(topic)
+		print("subscribe to {}.". format(topic))
 
 def on_message(client, userdata, msg):
+	from apps.taps.models import Tap
+	from apps.machines.models import Machine
+
 	print("Yeay! payload = "+str(msg.payload))
 	msg.payload = msg.payload.decode("utf-8")
-	
+
 	for machine in machines:
 		topic = "{}/state/carduid".format(machine.id)
 		if msg.topic == topic:
 			print(msg.topic+" "+str(msg.payload))
-			card_uid = msg.payload
-			src_machine = Machine.objects.get(pk=machine.id)
+			card_uid = str(msg.payload)
+			src_machine = Machine.objects.get(pk=1)
 			tap_time = timezone.now()
 			power_usage = 100
 			tap = Tap(card_uid = card_uid, machine = src_machine, tap_time = tap_time, power_usage = power_usage)
