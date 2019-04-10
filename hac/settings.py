ca53716f4db3962fcf9c1bc89d9e4f7a78717acf
vincent <vincentalfred1@gmail.com>
2019-04-10 15:51:33 +0700

    update setting

--- hac/settings.py
+++ hac/settings.py
@@ -82,26 +82,26 @@ WSGI_APPLICATION = 'hac.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-	# 'default': {
-	# 	'ENGINE': 'django.db.backends.mysql',
-	# 	'HOST': os.environ['DB_HOST'],
-	# 	# 'PORT': os.environ['DB_PORT'],
-	# 	'NAME': os.environ['DB_NAME'],
-	# 	'USER': os.environ['DB_USER'],
-	# 	'PASSWORD': os.environ['DB_PASSWORD'],
-	#   }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'HOST': os.environ['DB_HOST'],
+		# 'PORT': os.environ['DB_PORT'],
+		'NAME': os.environ['DB_NAME'],
+		'USER': os.environ['DB_USER'],
+		'PASSWORD': os.environ['DB_PASSWORD'],
+	  }
 #     'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': 'mydatabase',
 #    }
-   'default': {
-       'ENGINE': 'django.db.backends.mysql',
-       'HOST': '127.0.0.1',
-       'PORT': 3306,
-       'NAME': 'hac',
-       'USER': 'root',
-       'PASSWORD': 'root',
-   }
+   # 'default': {
+   #     'ENGINE': 'django.db.backends.mysql',
+   #     'HOST': '127.0.0.1',
+   #     'PORT': 3306,
+   #     'NAME': 'hac',
+   #     'USER': 'root',
+   #     'PASSWORD': 'root',
+   # }
 }
 
 
